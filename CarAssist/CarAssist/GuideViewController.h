5bac739167ad6db9f35423bcac607be5b3e8773f
Dennis Holst <dholst@refluxx.de>
2012-12-02 16:30:07 +0100

    GuideViewController erstellt

---
+++ CarAssist/CarAssist/GuideViewController.h
@@ -0,0 +1,13 @@
+//
+//  GuideViewController.h
+//  CarAssist
+//
+//  Created by Dennis on 02.12.12.
+//  Copyright (c) 2012 Gruppe Fear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuideViewController : UIViewController
+@property (nonatomic) IBOutlet UIScrollView *scrollView;
+@end
