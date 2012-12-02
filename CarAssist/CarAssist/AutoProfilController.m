5061ffc694d8f2868c1deba23c934c2392538b06
0fiedler <0fiedler@rzmac30.informatik.uni-hamburg.de>
2012-12-02 13:39:31 +0100

    Interface für Profil erstellt

---
+++ CarAssist/CarAssist/AutoProfilController.m
@@ -0,0 +1,38 @@
+//
+//  AutoProfilController.m
+//  CarAssist
+//
+//  Created by 0fiedler on 02.12.12.
+//  Copyright (c) 2012 Gruppe Fear. All rights reserved.
+//
+
+#import "AutoProfilController.h"
+
+@interface AutoProfilController ()
+
+@end
+
+@implementation AutoProfilController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
