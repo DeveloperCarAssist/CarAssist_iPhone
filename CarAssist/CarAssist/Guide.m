94e4bd7e4edfc6d203ed64f1663984d5ea7fe59e
Swantje Thüring <swantje-t@web.de>
2012-12-02 14:12:07 +0100

    Datenmodell für Guide und Step

---
+++ CarAssist/CarAssist/Guide.m
@@ -0,0 +1,13 @@
+//
+//  Guide.m
+//  CarAssist
+//
+//  Created by 0thuerin on 02.12.12.
+//  Copyright (c) 2012 Gruppe Fear. All rights reserved.
+//
+
+#import "Guide.h"
+
+@implementation Guide
+
+@end
