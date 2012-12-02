c0a30ce992dbe1ef72dbf4a15ee6eafdbf6e570b
Swantje Thüring <swantje-t@web.de>
2012-12-02 13:29:37 +0100

    ServiceCategoryViewController Stub aufgenommen. Service in Tabbar aufgenommen

---
+++ CarAssist/CarAssist/ServiceCategoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  ServiceCategoryViewController.h
+//  CarAssist
+//
+//  Created by 0thuerin on 02.12.12.
+//  Copyright (c) 2012 Gruppe Fear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServiceCategoryViewController : UIViewController
+
+@end
---
+++ CarAssist/CarAssist/ServiceCategoryViewController.m
@@ -0,0 +1,38 @@
+//
+//  ServiceCategoryViewController.m
+//  CarAssist
+//
+//  Created by 0thuerin on 02.12.12.
+//  Copyright (c) 2012 Gruppe Fear. All rights reserved.
+//
+
+#import "ServiceCategoryViewController.h"
+
+@interface ServiceCategoryViewController ()
+
+@end
+
+@implementation ServiceCategoryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
