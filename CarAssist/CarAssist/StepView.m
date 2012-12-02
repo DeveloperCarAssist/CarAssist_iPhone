011e700fca9b566cfcc220fc564b930f0ab3ad49
Swantje Thüring <swantje-t@web.de>
2012-12-02 16:28:44 +0100

    StepView angelegt

---
+++ CarAssist/CarAssist/StepView.m
@@ -0,0 +1,24 @@
+//
+//  StepView.m
+//  CarAssist
+//
+//  Created by 0thuerin on 02.12.12.
+//  Copyright (c) 2012 Gruppe Fear. All rights reserved.
+//
+
+#import "StepView.h"
+
+@implementation StepView
+
+-(id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if(self)
+    {
+        
+    }
+    
+    return self;
+}
+
+@end
