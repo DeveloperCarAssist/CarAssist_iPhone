74ed9b50de954f1bf0a0986394b85de7344c075e
0witt <0witt@rzmac28.informatik.uni-hamburg.de>
2012-12-02 14:14:22 +0100

    ServiceGuideStockService hinzugefügt

---
+++ CarAssist/CarAssist/ServiceGuideStockService.h
@@ -0,0 +1,15 @@
+//
+//  ServiceGuideStockService.h
+//  CarAssist
+//
+//  Created by 0witt on 02.12.12.
+//  Copyright (c) 2012 Gruppe Fear. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServiceGuideStockService : NSObject
+
+@property (nonatomic) NSDictionary* guides;
+
+@end
