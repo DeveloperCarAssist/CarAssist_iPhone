db6cf98508336bdda2e837f24d4352258cb28b15
Dennis Holst <dholst@refluxx.de>
2012-12-12 00:30:42 +0100

    Auswahl des favorisierten Wagens durch Tap auf das Favoriten-Icon möglich

--- CarAssist/CarAssist/CarFavoritViewController.m
+++ CarAssist/CarAssist/CarFavoritViewController.m
@@ -76,12 +76,26 @@
     return self.profil.carList.count;
 }
 
+// Ausgelöst vom Favoriten-Button...
+-(void)changeDefaultCar:(id)sender event:(id)event
+{
+    // die dem Button zugeordnete Zelle und den Indexpath abfragen, dann
+    // den Standardwagen setzen
+    NSSet *touches = [event allTouches];
+    UITouch *touch = [touches anyObject];
+    CGPoint currentTouchPosition = [touch locationInView:self.carFavoriteTableView];
+    NSIndexPath *indexPath = [self.carFavoriteTableView indexPathForRowAtPoint: currentTouchPosition];
+    if (indexPath != nil)
+    {
+        Car *car = [self.profil.carList objectAtIndex: indexPath.row];
+        self.profil.car = car;
+        [self.carFavoriteTableView reloadData];
+    }
+    
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UIImage* favoriteCar = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"favorit" ofType:@"png"]];
-    
-    UIImage* noFavoriteCar = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"kein_favorit" ofType:@"png"]];
-    
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
@@ -89,21 +103,34 @@
     }
   
     Car *car = [self.profil.carList objectAtIndex: indexPath.row];
-    NSString *string = [NSString stringWithFormat:@" %@ - %@ ",car.model, car.owner ];
-
     
-        
-    cell.textLabel.text= string;
+    // "Favorit setzen" Buttons erzeugen
+    CGFloat cellWidth = cell.frame.size.width;
+    CGFloat cellHeight = cell.frame.size.height - 2.0f; // ein wenig kleiner damit die Rahmenlinien nicht überzeichnet werden
+    UIImage* favoriteCar = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"favorit" ofType:@"png"]];
+    UIImage* noFavoriteCar = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"kein_favorit" ofType:@"png"]];
+    UIButton *changeDefaultCarButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [changeDefaultCarButton addTarget:self action:@selector(changeDefaultCar:event:) forControlEvents:UIControlEventTouchDown];
+    changeDefaultCarButton.frame = CGRectMake(10.0f, 1.0f, cellHeight,cellHeight);
+
+    // Zelle beschriften. cell.textLabel kann hier nicht verwendet werden, weil
+    // der Button den Text überlappen würde. Deshalb erzeugen wir unser eigenes Label
+    // und bestimmen die Anzeigeposition selbst.
+    UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake(cellHeight + 20.0f,1.0f,cellWidth - cellHeight - 10.0f, cellHeight)];
+    textLabel.text =  [NSString stringWithFormat:@" %@ - %@ ",car.model, car.owner ];
+    
     if (car == self.profil.car)
     {
-        [cell.imageView setImage:favoriteCar];
-        //[cell.imageView setImage: asdasd]; Image für Ausgewähltes Auto
+        [changeDefaultCarButton setImage:favoriteCar forState:UIControlStateNormal];
+        textLabel.font = [UIFont boldSystemFontOfSize:16.0f];
     }
     else
     {
-        [cell.imageView setImage:noFavoriteCar];
-     //       [cell.imageView setImage:breakdownServiceCategoryImage]; Image für nicht ausgewählte Autos
+        [changeDefaultCarButton setImage:noFavoriteCar forState:UIControlStateNormal];
+        textLabel.font = [UIFont systemFontOfSize:16.0f];
     }
+    [cell addSubview:changeDefaultCarButton];
+    [cell addSubview:textLabel];
     
     return cell;
 }
