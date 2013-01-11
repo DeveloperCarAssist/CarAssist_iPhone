76e674b3a1219d96d411ed5afc835fb71743cb76
0fiedler <0fiedler@rzmac29.informatik.uni-hamburg.de>
2013-01-11 16:08:51 +0100

    Einfacher Button zum Anrufen in den Warnings

--- CarAssist/CarAssist/WarningLightViewController.m
+++ CarAssist/CarAssist/WarningLightViewController.m
@@ -9,6 +9,10 @@
 #import <QuartzCore/QuartzCore.h>
 #import "WarningLightViewController.h"
 #import "Utils.h"
+#import <MessageUI/MFMailComposeViewController.h>
+#import <MapKit/MapKit.h>
+#import "CallViewController.h"
+
 
 @interface WarningLightViewController ()
 
@@ -42,6 +46,60 @@
     imgView.layer.cornerRadius = 15.0;
     imgView.layer.masksToBounds = YES;
     
+    UIBarButtonItem* callButton = [[UIBarButtonItem alloc] initWithTitle: @"Hilfe!" style:UIBarButtonItemStyleBordered target:self action: @selector(callAdac)];
+    [self.navigationItem setRightBarButtonItem: callButton];
+    
 }
+
+-(void)callAdac
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"ADAC kontaktieren" message:@"Wollen Sie den ADAC anrufen und damit Ihre Daten und Ihren Standord an den ADAC senden?" delegate:self cancelButtonTitle:@"Abbrechen" otherButtonTitles:@"Ja, ich will: Telefon!", @"Ja, ich will: Email und Anrufen!",nil];
+    [alert show];
+}
+
+-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    
+    if([alertView.title isEqual: @"ADAC kontaktieren"])
+    {
+        if(buttonIndex == 1)
+        {
+            if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tel:040555555"]])
+            {
+                
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel:040555555"]];
+            }
+            else
+            {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Kein Telefon" message:@"Diese Funktion benötigt Zugriff zum Telefonieren. Bitte erlauben sie dies." delegate:Nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+                [alert show];
+            }
+        }
+        if(buttonIndex == 2)
+        {
+            if ([MFMailComposeViewController canSendMail]) {
+                
+                if(![CLLocationManager locationServicesEnabled])
+                {
+                    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Hinweis" message:@"Um ein Ortungssignal anzugeben muss das GPS aktiviert sein." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [message show];
+                }
+                CallViewController* viewController = [[CallViewController alloc] initForMail];
+                [self.navigationController pushViewController:viewController animated:NO];
+            }
+            else {
+                
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Failure"
+                                                                message:@"Your device doesn't support the composer sheet"
+                                                               delegate:nil
+                                                      cancelButtonTitle:@"OK"
+                                                      otherButtonTitles: nil];
+                [alert show];
+                
+            }
+        }
+    }
+}
+
 
 @end
