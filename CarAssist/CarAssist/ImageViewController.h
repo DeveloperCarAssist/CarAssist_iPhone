56b9ce61858aa79824e81430cba950abb8e6a31d
Dennis Michielse <dennis.michielse@googlemail.com>
2013-01-12 15:47:50 +0100

    An Kamera rumexperimentiert (keine vorhandenen Änderungen!)

---
+++ CarAssist/CarAssist/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  CarAssist
+//
+//  Created by 0michiel on 12.01.13.
+//  Copyright (c) 2013 Gruppe Fear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
