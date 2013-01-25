99eb20283346c41dcde3445a6922696f72868a41
0fiedler <0fiedler@rzmac19.informatik.uni-hamburg.de>
2013-01-25 15:37:25 +0100

    Service Angepasst

--- CarAssist/CarAssist/ServiceGuideStockService.m
+++ CarAssist/CarAssist/ServiceGuideStockService.m
@@ -25,15 +25,15 @@
         {//audi, bmw golf
             case 1:
                 //      [self initServiceGuidesWithExampleDataAudi];
-                [self initGuidesForUXTest];
+                [self initGuidesForAudi];
                 [self initDictionary];
                 break;
             case 2:
-                [self initServiceGuidesWithExampleDataBMW];
+                [self initServiceGuidesForBMW];
                 [self initDictionary];
                 break;
             case 3:
-                [self initServiceGuidesWithExampleDataVWGolfIV];
+                [self initServiceGuidesForVWGolfIV];
                 [self initDictionary];
                 break;
             default:
@@ -48,107 +48,15 @@
  * Initialisiert (noch hart gecoded) die Schritt-für-Schritt-Anleitungen der Servicerubrik für das 1. Testfahrzeug
  *
  */
-- (void) initServiceGuidesWithExampleDataBMW
+- (void) initServiceGuidesForBMW
 {
     NSMutableArray* allGuides = [NSMutableArray array];
-    NSMutableArray* steps = [NSMutableArray array];
+    //NSMutableArray* steps = [NSMutableArray array];
+
+    [allGuides addObject:[self ReserveReifenWechseln]];   
+    [allGuides addObject: [self BMWOelstand]];
+    [allGuides addObjectsFromArray:[self DummyGuides]];
     
-    UIImage* image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"reifenwechsel_schritt_10" ofType:@"jpeg"]];
-    Step* step = [[Step alloc] initWithName: @"Reifenwechel" Description:@"Sie benötigen: \n Radkreuz \n Wagenheber \n" AndImage:image];
-    [steps addObject:step];
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"reifenwechsel_schritt_10" ofType:@"jpeg"]];
-    step = [[Step alloc] initWithName: @"Radmuttern lösen" Description:@"Verwenden Sie das Radkreuz zum Lösen der Radmuttern. Wechseln Sie nur einen Reifen zur Zeit. Um mehr Kraft aufzuwenden, verwenden Sie Ihren Fuß als Hebel. Lösen Sie die Radmuttern nur leicht an." AndImage:image];
-    [steps addObject:step];
-    /* alter Text
-     "Als ersten sollten sie den Reservereifen holen.\n Dazu begeben Sie sich bitte zu Ihrem Kofferaum und öffnen diesen.\n Räumen Sie den Kofferaum leer.\n Unter der Abdeckung finden Sie Ihren Reservereifen und einen Schlüssel zum lösen der Radmuttern. Gegebenenfalls ist auch ein Felgenschloss dabei.\n Nehmen Sie den Reservereifen aus der Vertiefung."
-     */
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"reifenwechsel_schritt_20" ofType:@"jpeg"]];
-    step = [[Step alloc] initWithName: @"Wagenheber ansetzen" Description:@"Setzen Sie den Wagenheber an der Karosserie markierten Stelle an. Heben Sie den Wagen nur leicht an, bis das Rad knapp über dem Boden schwebt." AndImage:image];
-    [steps addObject:step];
-    
-    step = [[Step alloc] initWithName: @"Info" Description:@"In dieser Anleitung befinden sich Texte für den BMW." AndImage:nil ];
-    [steps addObject:step];
-    
-    /* alter Text
-     Als erstes müssen Sie die das Auto aufbocken. Dann lösen Sie die Radmuttern des alten Reifens und nehmen den Reifen ab (Legen Sie den Reifen am besten in de Kofferaum, damit Sie ihn nicht vergessen). Jetzt nur noch den Reservereifen wieder anschrauben! Fahren Sie nicht zu schnell. Der Reservereifen ist nur für Geschwindigkeiten bis 90 Kilometer pro String ausgelegt!
-     */
-    
-    
-    Guide* guide = [[Guide alloc] initWithName: @"Reservereifen montieren" CategoryName: @"Reifen" AndSteps: steps];
-    [allGuides addObject:guide];
-    
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    
-    steps = [NSMutableArray array];
-    
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"kuehlwasser" ofType:@"jpg"]];
-    
-    step = [[Step alloc] initWithName: @"Schritt 1: Kühlwassser kaufen" Description:@"Als ersten sollten sie den Reservereifen holen.\n Dazu begeben Sie sich bitte zu Ihrem Kofferaum und öffnen diesen.\n Räumen Sie den Kofferaum leer.\n Unter der Abdeckung finden Sie Ihren Reservereifen und einen Schlüssel zum lösen der Radmuttern. Gegebenenfalls ist auch ein Felgenschloss dabei.\n Nehmen Sie den Reservereifen aus der Vertiefung." AndImage:image];
-    [steps addObject:step];
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"nachfuellen" ofType:@"jpg"]];
-    step = [[Step alloc] initWithName: @"Schritt 2: Kühlwasser nachfüllen" Description:@"Als erstes müssen Sie die das Auto aufbocken. Dann lösen Sie die Radmuttern des alten Reifens und nehmen den Reifen ab (Legen Sie den Reifen am besten in de Kofferaum, damit Sie ihn nicht vergessen). Jetzt nur noch den Reservereifen wieder anschrauben! Fahren Sie nicht zu schnell. Der Reservereifen ist nur für Geschwindigkeiten bis 90 Kilometer pro String ausgelegt!" AndImage:image];
-    [steps addObject:step];
-    
-    guide = [[Guide alloc] initWithName: @"Kühlwasser nachfüllen" CategoryName: @"Motor" AndSteps: steps];
-    [allGuides addObject:guide];
-    
-    
-    
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    
-    steps = [NSMutableArray array];
-    
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"kuehlwasser" ofType:@"jpg"]];
-    
-    step = [[Step alloc] initWithName: @"Kühlwassser kaufen" Description:@"Als ersten sollten sie den Reservereifen holen.\n Dazu begeben Sie sich bitte zu Ihrem Kofferaum und öffnen diesen.\n Räumen Sie den Kofferaum leer.\n Unter der Abdeckung finden Sie Ihren Reservereifen und einen Schlüssel zum lösen der Radmuttern. Gegebenenfalls ist auch ein Felgenschloss dabei.\n Nehmen Sie den Reservereifen aus der Vertiefung." AndImage:image];
-    [steps addObject:step];
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"nachfuellen" ofType:@"jpg"]];
-    step = [[Step alloc] initWithName: @"Kühlwasser nachfüllen" Description:@"Als erstes müssen Sie die das Auto aufbocken. Dann lösen Sie die Radmuttern des alten Reifens und nehmen den Reifen ab (Legen Sie den Reifen am besten in de Kofferaum, damit Sie ihn nicht vergessen). Jetzt nur noch den Reservereifen wieder anschrauben! Fahren Sie nicht zu schnell. Der Reservereifen ist nur für Geschwindigkeiten bis 90 Kilometer pro String ausgelegt!" AndImage:image];
-    [steps addObject:step];
-    
-    guide = [[Guide alloc] initWithName: @"Kühlwasser kontrollieren" CategoryName: @"Motor" AndSteps: steps];
-    [allGuides addObject:guide];
-    
-    self.allItems = allGuides;
-    self.reducedItems = allGuides;
-    
-    
-    //------------------- BMW Ölstand echte anleitung ----
-    
-    steps = [NSMutableArray array];
-    
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"MotorHebel" ofType:@"JPG"]];
-    step = [[Step alloc] initWithName: @"Motorraumöffner finden" Description:@"Bei dem Gaspedalen links ist der abgebildete Hebel." AndImage:image];
-    [steps addObject:step];
-    
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"MotorHebelbetaetigen" ofType:@"JPG"]];
-    step = [[Step alloc] initWithName: @"Motorraumöffner betätigen" Description:@"Bei dem Gaspedalen links ist der abgebildete Hebel." AndImage:image];
-    [steps addObject:step];
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Motorhaube" ofType:@"JPG"]];
-    step = [[Step alloc] initWithName: @"Motorraum öffnen" Description:@"Drücken Sie den Hebel rein und heben sie danach die Moterhaube" AndImage:image];
-    [steps addObject:step];
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"OelStandmesserOrt" ofType:@"JPG"]];
-    step = [[Step alloc] initWithName: @"Ölstab rausziehen" Description:@"Ziehen sie an dem roten Ring um den Ölstab raus zu ziehen" AndImage:image];
-    [steps addObject:step];
-    
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Oelstab" ofType:@"JPG"]];
-    step = [[Step alloc] initWithName: @"Ölstab abwischen und erneut benetzten" Description:@"Wischen sie die Oelreste mit einem Taschentuch, stecken sie den Stab wieder in die Halterung und ziehen sie ihn erneut raus" AndImage:image];
-    [steps addObject:step];
-    
-    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Oelstab" ofType:@"JPG"]];
-    step = [[Step alloc] initWithName: @"Ölstab ablesen" Description:@"Sie können nun die Ölstand messen. Auf dem Stab befindet sich 2 Linien, min und max. Zwischen diesen beiden sollte der Ölstand liegen." AndImage:image];
-    [steps addObject:step];
-    
-    guide = [[Guide alloc] initWithName: @"Ölstand kontrollieren" CategoryName: @"Motor" AndSteps: steps];
-    [allGuides addObject:guide];
     
     self.allItems = allGuides;
     self.reducedItems = allGuides;
@@ -160,7 +68,7 @@
  * Initialisiert (noch hart gecoded) die Schritt-für-Schritt-Anleitungen der Servicerubrik für das 2. Testfahrzeug
  *
  */
-- (void) initServiceGuidesWithExampleDataVWGolfIV
+- (void) initServiceGuidesForVWGolfIV
 {
     NSMutableArray* allGuides = [NSMutableArray array];
     NSMutableArray* steps = [NSMutableArray array];
@@ -181,14 +89,63 @@
     Guide* guide = [[Guide alloc] initWithName: @"Reservereifen montieren" CategoryName: @"Reifen" AndSteps: steps];
     [allGuides addObject:guide];
     
+    [allGuides addObjectsFromArray: [self DummyGuides]];
+    
     self.allItems = allGuides;
     self.reducedItems = allGuides;
 }
 
-- (void) initGuidesForUXTest
+- (void) initGuidesForAudi
 {
     NSMutableArray* allGuides = [NSMutableArray array];
-    NSMutableArray* steps = [NSMutableArray array];
+    //NSMutableArray* steps = [NSMutableArray array];
+    
+    [allGuides addObject: [self ReserveReifenWechseln]];
+    
+    [allGuides addObjectsFromArray: [self DummyGuides]];
+    
+    self.allItems = allGuides;
+    self.reducedItems = allGuides;
+    
+}
+
+
+-(Guide*) BMWOelstand
+{
+   NSMutableArray* steps = [NSMutableArray array];    
+    
+   UIImage* image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"MotorHebel" ofType:@"JPG"]];
+    Step* step = [[Step alloc] initWithName: @"Motorraumöffner finden" Description:@"Im Fußraum des Fahrers ist links neben dem Gaspedal der abgebildete Hebel." AndImage:image];
+    [steps addObject:step];
+    
+    
+    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"MotorHebelbetaetigen" ofType:@"JPG"]];
+    step = [[Step alloc] initWithName: @"Motorraumöffner betätigen" Description:@"Ziehen Sie wie abgebildet am Hebel und lösen Sie so die Sperre der Motorhaube." AndImage:image];
+    [steps addObject:step];
+    
+    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Motorhaube" ofType:@"JPG"]];
+    step = [[Step alloc] initWithName: @"Motorraum öffnen" Description:@"Drücken Sie den Hebel rein und heben Sie danach die Moterhaube an, um an den Motorraum zu kommen." AndImage:image];
+    [steps addObject:step];
+    
+    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"OelStandmesserOrt" ofType:@"JPG"]];
+    step = [[Step alloc] initWithName: @"Ölstab rausziehen" Description:@"Ziehen Sie an dem roten Griff um den Ölstab zu entnehmen" AndImage:image];
+    [steps addObject:step];
+    
+    
+    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Oelstab" ofType:@"JPG"]];
+    step = [[Step alloc] initWithName: @"Ölstab abwischen und erneut benetzten" Description:@"Wischen Sie das Öl mit einem Tuch ab, stecken Sie den Stab wieder in die Halterung und ziehen Sie ihn erneut heraus." AndImage:image];
+    [steps addObject:step];
+    
+    image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Oelstab" ofType:@"JPG"]];
+    step = [[Step alloc] initWithName: @"Ölstand ablesen" Description:@"Sie können nun den Ölstand messen. Auf dem Stab befinden sich 2 Linien: min und max. Zwischen diesen beiden Linien sollte der Ölstand liegen. Wenn das nicht der Fall ist, füllen Sie Öl nach oder lassen Sie welchen ab. Hierfür gibt es ebenfalls Anleitungen (Motoröl nachfüllen, Motoröl ablassen)." AndImage:image];
+    [steps addObject:step];
+    
+    return [[Guide alloc] initWithName: @"Ölstand kontrollieren" CategoryName: @"Motor" AndSteps: steps];
+}
+
+-(Guide*) ReserveReifenWechseln
+{
+     NSMutableArray* steps = [NSMutableArray array];
     
     UIImage* image = [UIImage  imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"used" ofType:@"jpg"]];
     Step* step = [[Step alloc] initWithName: @"Was Sie brauchen" Description:@"Sie benötigen (v.l.n.r) Reservereifen, Wagenkreuz, Kreutz-Schraubenzieher, Felgenschloss, Wagenheber und optional einen Drehmomentschlüssel. Diese Dinge sollten Sie unter der Kofferaum abdeckung finden." AndImage:image];
@@ -238,57 +195,78 @@
     step = [[Step alloc] initWithName: @"Gute Fahrt" Description:@"Fahren Sie mit dem Reservereifen nicht über 80 kmh und suchen Sie schnellstmöglich eine Werkstatt auf. Ihr CarAssist Team wünscht eine gute Fahrt!" AndImage:image ];
     [steps addObject:step];
     
-    Guide* guide = [[Guide alloc] initWithName: @"Reservereifen wechseln" CategoryName: @"Reifen" AndSteps: steps];
+    return [[Guide alloc] initWithName: @"Reservereifen wechseln" CategoryName: @"Reifen" AndSteps: steps];
+
+}
+
+-(NSArray*)DummyGuides
+{
+    NSMutableArray* steps = [NSMutableArray array];
+     NSMutableArray* allGuides = [NSMutableArray array];
+    Guide* guide;
+    
+    guide = [[Guide alloc] initWithName: @"Motoröl nachfüllen" CategoryName: @"Motor" AndSteps: steps];
+      [allGuides addObject:guide];
+    guide = [[Guide alloc] initWithName: @"Motoröl ablassen" CategoryName: @"Motor" AndSteps: steps];
     [allGuides addObject:guide];
     
-    
-    
-    // ____________________________________________________Dummy-Guides-to-fill-List______________________________________________________________________________________
-    
-    
-    steps = [NSMutableArray array];
-
-    guide = [[Guide alloc] initWithName: @"Ölstand messen" CategoryName: @"Motor" AndSteps: steps];
-    [allGuides addObject:guide];
-
     guide = [[Guide alloc] initWithName: @"Kühlwasser wechseln" CategoryName: @"Motor" AndSteps: steps];
-    [allGuides addObject:guide];
-
-    guide = [[Guide alloc] initWithName: @"Motor tauschen" CategoryName: @"Motor" AndSteps: steps];
     [allGuides addObject:guide];
     
     guide = [[Guide alloc] initWithName: @"Überbrücken" CategoryName: @"Motor" AndSteps: steps];
     [allGuides addObject:guide];
-
+    
+    guide = [[Guide alloc] initWithName: @"Batterie wechseln" CategoryName: @"Motor" AndSteps: steps];
+    [allGuides addObject:guide];
+    
+    guide = [[Guide alloc] initWithName: @"Bremsflüssigkeit prüfen" CategoryName: @"Motor" AndSteps: steps];
+    [allGuides addObject:guide];
+    guide = [[Guide alloc] initWithName: @"Bremsflüssigkeit wechseln" CategoryName: @"Motor" AndSteps: steps];
+    [allGuides addObject:guide];
+    
     guide = [[Guide alloc] initWithName: @"Scheibenwischwasser nachfüllen" CategoryName: @"Scheiben" AndSteps: steps];
     [allGuides addObject:guide];
-
+    
     guide = [[Guide alloc] initWithName: @"Scheibenwischblätter wechseln" CategoryName: @"Scheiben" AndSteps: steps];
     [allGuides addObject:guide];
-
-    guide = [[Guide alloc] initWithName: @"Scheibe säubern" CategoryName: @"Scheibe" AndSteps: steps];
+    
+    guide = [[Guide alloc] initWithName: @"Scheiben reinigen" CategoryName: @"Scheiben" AndSteps: steps];
     [allGuides addObject:guide];
-
-    guide = [[Guide alloc] initWithName: @"Scheibe enteisen" CategoryName: @"Scheibe" AndSteps: steps];
+    
+    guide = [[Guide alloc] initWithName: @"Scheibe enteisen" CategoryName: @"Scheiben" AndSteps: steps];
     [allGuides addObject:guide];
-
-    guide = [[Guide alloc] initWithName: @"Lampe wechseln" CategoryName: @"Beleuchtung" AndSteps: steps];
+    
+   guide = [[Guide alloc] initWithName: @"Frontstrahler wechseln" CategoryName: @"Beleuchtung" AndSteps: steps];
     [allGuides addObject:guide];
     
     guide = [[Guide alloc] initWithName: @"Winterreifen wechseln" CategoryName: @"Reifen" AndSteps: steps];
     [allGuides addObject:guide];
     
+    guide = [[Guide alloc] initWithName: @"Reifendruck Prüfen" CategoryName: @"Reifen" AndSteps: steps];
+    [allGuides addObject:guide];
+    
     guide = [[Guide alloc] initWithName: @"Reifen aufpumpen" CategoryName: @"Reifen" AndSteps: steps];
     [allGuides addObject:guide];
-
     
-    // ____________________________________________________Dummy-Guides-to-fill-List______________________________________________________________________________________
+    guide = [[Guide alloc] initWithName: @"Nebelschlussleuchte wechseln" CategoryName: @"Beleuchtung" AndSteps: steps];
+    [allGuides addObject:guide];
     
+    guide = [[Guide alloc] initWithName: @"Blinkerlampe wechseln" CategoryName: @"Beleuchtung" AndSteps: steps];
+    [allGuides addObject:guide];
     
-    self.allItems = allGuides;
-    self.reducedItems = allGuides;
+    guide = [[Guide alloc] initWithName: @"Sitze reinigen" CategoryName: @"Innenraum" AndSteps: steps];
+    [allGuides addObject:guide];
     
+    guide = [[Guide alloc] initWithName: @"Amaturenbrett reinigen" CategoryName: @"Innenraum" AndSteps: steps];
+    [allGuides addObject:guide];
+    
+    guide = [[Guide alloc] initWithName: @"Boden reinigen" CategoryName: @"Innenraum" AndSteps: steps];
+    [allGuides addObject:guide];
+    
+    guide = [[Guide alloc] initWithName: @"Lüftungssystem Reinigen" CategoryName: @"Innenraum" AndSteps: steps];
+    [allGuides addObject:guide];
+    
+    return allGuides;
 }
-
 
 @end
